fe44a129b70e6055690a1e88df20f90a945d3e0a
crbiitr <crbiitr@gmail.com>
2018-05-17 20:53:17 +0530

    Added new code

---
+++ Testing/src/Mars_Rover.java
@@ -0,0 +1,93 @@
+/**
+ * Created by chetan on 10/2/18.
+ */
+import java.util.Scanner;
+
+public class Mars_Rover {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int x, y;
+        int endx = scan.nextInt();
+        int endy = scan.nextInt();
+
+        x = scan.nextInt();
+        y = scan.nextInt();
+        char facing = scan.next().charAt(0);
+        char control[] = scan.next().toCharArray();
+        Rover r = new Rover(endx, endy);
+        Position pos = r.findPosition(x, y, facing, control);
+        System.out.println(pos.x + " "+ pos.y+" "+ facing);
+        scan.close();
+    }
+}
+
+class Rover{
+    int endx, endy;
+    Rover(int endx, int endy){
+        this.endx = endx;
+        this.endy = endy;
+    }
+    Position findPosition(int x, int y, char facing, char control[]){
+        Position pos;
+        pos = new Position(x, y, facing);
+        for (int i = 0; i < control.length; i++) {
+
+            if(pos.facing == 'N' && control[i] == 'L'){
+                pos.facing = 'W';
+            }
+
+            else if(pos.facing == 'W' && control[i] == 'L'){
+                pos.facing = 'S';
+            }
+
+            else if(pos.facing == 'S' && control[i] == 'L'){
+                pos.facing = 'E';
+            }
+
+            else if(pos.facing == 'E' && control[i] == 'L'){
+                pos.facing = 'N';
+            }
+
+            else if(pos.facing == 'N' && control[i] == 'R'){
+                pos.facing = 'E';
+            }
+
+            else if(pos.facing == 'E' && control[i] == 'R'){
+                pos.facing = 'S';
+            }
+
+            else if(pos.facing == 'S' && control[i] == 'R'){
+                pos.facing = 'W';
+            }
+
+            else if(pos.facing == 'W' && control[i] == 'R'){
+                pos.facing = 'N';
+            }
+            if(control[i] == 'M'){
+                pos = moveRover(pos.x, pos.y, pos.facing);
+//	System.out.println(pos.x + " "+pos.y+" "+facing);
+            }
+        }
+        return pos;
+    }
+
+    Position moveRover(int x, int y, char facing){
+        if (facing == 'N' && y<endy) {="" y++;="" }else="" if(facing="=" 's'="" &&="" y="">0 ){
+            y--;
+        } else if(facing == 'E' && x<endx){ x++;="" }="" else="" if(facing="=" 'w'="" &&="" x="">0){
+            x--;
+        }
+        return new Position(x, y, facing);
+    }
+
+}
+
+class Position{
+    int x, y;
+    char facing;
+    Position(int x, int y, char facing){
+        this.x = x;
+        this.y = y;
+        this.facing = facing;
+    }
+}
--- Testing/src/Solution.java
+++ Testing/src/Solution.java
@@ -6,264 +6,178 @@ import java.util.*;
  *https://www.geeksforgeeks.org/check-whether-a-given-point-lies-inside-a-triangle-or-not/
  */
 public class Solution {
-   /* public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int n  = in.nextInt();
-        String a = in.nextLine();
-        System.out.println(steadyGene(a));
-    }*/
-   public static void main(String[] args) {
-//       int[] arr = {1,1,0,1,0,1,1,1};
-//       int[] arr = {1,1,0,1,0,1,0,1,0,0,1};
-       int [] arr={1,1,0,1,0,1,1,1};
-        String str = "11010101001";
-       System.out.println(getLongestBalancedChromosome(str));
-       System.out.println(findMaxLength(arr));
+   /* static int pointsBelongToTriangle(int x1, int y1, int x2, int y2, int x3, int y3, int xP, int yP, int xQ, int yQ) {
+        boolean isPExist = false, isQExist=false;
+        //Check if triangle can form
+        if(isTriangleExist(x1,y1,x2,y2,x3,y3)!=0) {
+            if (pointBelongsToTriangle(x1,y1,x2,y2,x3,y3,xP,yP))
+                isPExist = true;
+            else
+                isPExist = false;
 
-   }
-    public static int findMaxLength(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>();
-        map.put(0, -1);
-        int maxlen = 0, count = 0;
-        for (int i = 0; i < nums.length; i++) {
-            count = count + (nums[i] == 1 ? 1 : -1);
-            if (map.containsKey(count)) {
-                maxlen = Math.max(maxlen, i - map.get(count));
-            } else {
-                map.put(count, i);
-            }
+            if (pointBelongsToTriangle(x1,y1,x2,y2,x3,y3,xQ,yQ))
+                isQExist = true;
+            else
+                isQExist = false;
+
+            if(isPExist && isQExist)
+                return 3;
+            else if(isPExist && !isQExist)
+                return 1;
+            else if(!isPExist && isQExist)
+                return 2;
+            else
+                return 4;
+
+        } else {
+            return 0;
         }
-        return maxlen;
     }
-    public static int  getLongestBalancedChromosome(String c)
+
+    static int isTriangleExist(int x1, int y1, int x2, int y2, int x3, int y3)
     {
-        if(c == null || c.length()==1) return 0;
-        int sum = 0;
-        int maxsize = -1, startindex = 0;
-        int endindex = 0;
-
-        int n = c.length();
-        char[] arr = c.toCharArray();
-
-        // Pick a starting point as i
-
-        int []temp = new int[n];
-        for (int i = 1; i < n; i++)
-        {
-            temp[i] = Math.abs(temp[i]-temp[i-1]);
-        }
-        return count(temp);
-    }
-    
-    public static int count(int[] arr) {
-       int count = 0;
-        int maxCount = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if(arr[i]==1 && arr[i+1]==1) {
-                count++;
-
-            } else {
-                count=0;
-            }
-            if(maxCount<count) maxCount=count;
-
-        }
-        return maxCount;
+        return x1*(y2 - y3) - y1*(x2 - x3) + 1*(x2*y3 - y2*x3);
     }
 
-    /*static int getLongestBalancedChromosome(String c) {
-        int length = c.length();
-        if(c==null ||length==0) return 0;
-        Stack<Integer> stack = new Stack<>();
-        stack.push(-1);
-        int result = 0;
-        for (int i = 0; i < length; i++) {
-            if (c.charAt(i) == '0') {
-                stack.push(i);
-            } else {
-                stack.pop();
-                if (!stack.empty())
-                    result = Math.max(result, i - stack.peek());
-                else stack.push(i);
-            }
-        }
-        return result;
+    static double area(int x1, int y1, int x2, int y2, int x3, int y3)
+    {
+        return Math.abs((x1*(y2-y3) + x2*(y3-y1)+ x3*(y1-y2))/2.0);
+    }
+
+    static boolean pointBelongsToTriangle(int x1, int y1, int x2, int y2, int x3, int y3, int x, int y)
+    {
+        double A = area (x1, y1, x2, y2, x3, y3);
+
+        double A1 = area (x, y, x2, y2, x3, y3);
+
+        double A2 = area (x1, y1, x, y, x3, y3);
+
+        double A3 = area (x1, y1, x2, y2, x, y);
+
+        return (A == A1 + A2 + A3);
+    }*/
+
+    /*public static void main(String[] args) {
+        *//*Scanner in = new Scanner(System.in);
+        int l = in.nextInt();
+        String s = in.next();*//*
+//        int result = twoCharaters(s);?
+        int arr[] = {3,3,4,2};
+        System.out.println(maxPoints(arr));
+
+//        in.close();
+    }*/
+/*
+
+    public static final String YES = "YES";
+    public static final String NO = "NO";
+    static String isPossible(int a, int b, int c, int d) {
+        if(a>c || b>d) return NO;
+        if(a==c && b==d) return YES;
+
+        String left = isPossible(a+b,b,c,d);
+        String right = isPossible(a,a+b,c,d);
+
+        if(left.equals(YES) || right.equals(YES))
+            return YES;
+
+        return NO;
     }
 */
 
-    static int qw(String c) {
+    /*public static long maxPoints(int[] elements) {
+        int[] count = new int[100001];
+        for (int num: elements) count[num]++;
+        long remove = 0;
+        long use = 0;
+        int prev = -1;
 
-        if(c == null || c.length()==1) return 0;
-        int n = c.length();
-        char[] array = c.toCharArray();
-        int tempArray[][] = new int[n][2];
-        for (int i = 0; i < n; i++)
-            tempArray[i][0] = tempArray[i][1] = 1;
-
-        int result = 1;
-
-        for (int i = 1; i < n; i++)
-        {
-            for (int j = 0; j < i; j++)
-            {
-                if (array[j] < array[i] &&
-                        tempArray[i][0] < tempArray[j][1] + 1)
-                    tempArray[i][0] = tempArray[j][1] + 1;
-
-                if( array[j] > array[i] &&
-                        tempArray[i][1] < tempArray[j][0] + 1)
-                    tempArray[i][1] = tempArray[j][0] + 1;
+        for (int i = 0; i <= 100000; ++i) if (count[i] > 0) {
+            long max = Math.max(remove, use);
+            if (i - 1 != prev) {
+                use = i * count[i] + max;
+                remove = max;
+            } else {
+                use = i * count[i] + remove;
+                remove = max;
             }
-
-            if (result < Math.max(tempArray[i][0], tempArray[i][1]))
-                result = Math.max(tempArray[i][0], tempArray[i][1]);
+            prev = i;
         }
-        return result-1;
+        return Math.max(remove, use);
+    }*/
+
+    /*static long maxPoints(int[] elements) {
+
+    }*/
+
+    public static final Integer N = 1;
+    public static final Integer E = 2;
+    public static final Integer S = 3;
+    public static final Integer W = 4;
+    Integer x = 0;
+    Integer y = 0;
+    Integer facing = N;
+    public Solution() {
     }
-/*
-    public static int  getLongestBalancedChromosome(String c)
-    {
-        if(c == null || c.length()==1) return 0;
-        int sum = 0;
-        int maxsize = -1, startindex = 0;
-        int endindex = 0;
-
-        int n = c.length();
-        char[] arr = c.toCharArray();
-
-        // Pick a starting point as i
-
-        for (int i = 0; i < n - 1; i++)
-        {
-            sum = (arr[i] == 0) ? -1 : 1;
-
-            // Consider all subarrays starting from i
-
-            for (int j = i + 1; j < n; j++)
-            {
-                if(arr[j] == 0)
-                    sum += -1;
-                else
-                    sum += 1;
-
-                // If this is a 0 sum subarray, then
-                // compare it with maximum size subarray
-                // calculated so far
-
-                if (sum == 0 && maxsize < j - i + 1)
-                {
-                    maxsize = j - i + 1;
-                    startindex = i;
-                }
-            }
-        }
-        endindex = startindex+maxsize-1;
-        if (maxsize == -1)
-            System.out.println("No such subarray");
-        else
-            System.out.println(startindex+" to "+endindex);
-
-        return maxsize;
-    }*/
-
-    /*private static String transform(String input, int rot, int position, int len) {
-        char[] ar = input.toCharArray();
-        if (rot % 2 != 0) {
-            for (int i = 0; i < rot; i++) {
-                rotate(ar, position - 1, len - position);
-            }
-        }
-        return String.valueOf(ar);
-    }*/
-    public static int maxLen(int arr[], int n)
-    {
-        // Creates an empty hashMap hM
-
-        HashMap<Integer, Integer> hM = new HashMap<Integer, Integer>();
-
-        int sum = 0;     // Initialize sum of elements
-        int max_len = 0; // Initialize result
-        int ending_index = -1;
-        int start_index = 0;
-
-        for (int i = 0; i < n; i++)
-        {
-            arr[i] = (arr[i] == 0) ? -1 : 1;
-        }
-
-        // Traverse through the given array
-
-        for (int i = 0; i < n; i++)
-        {
-            // Add current element to sum
-
-            sum += arr[i];
-
-            // To handle sum=0 at last index
-
-            if (sum == 0)
-            {
-                max_len = i + 1;
-                ending_index = i;
-            }
-
-            // If this sum is seen before, then update max_len
-            // if required
-
-            if (hM.containsKey(sum))
-            {
-                if (max_len < i - hM.get(sum + n))
-                {
-                    max_len = i - hM.get(sum + n);
-                    ending_index = i;
-                }
-            }
-            else // Else put this sum in hash table
-                hM.put(sum + n, i);
-        }
-
-        for (int i = 0; i < n; i++)
-        {
-            arr[i] = (arr[i] == -1) ? 0 : 1;
-        }
-
-        int end = ending_index - max_len + 1;
-        System.out.println(end + " to " + ending_index);
-
-        return max_len;
+    public void setPosition(Integer x, Integer y, Integer facing) {
+        this.x = x;
+        this.y = y;
+        this.facing = facing;
     }
-
-/*
-    public class Solution {
-        public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
-            String input = scanner.next();
-            int rot = scanner.nextInt();
-            int position = scanner.nextInt();
-            int len = input.length();
-            System.out.print(transform(input, rot, position, len));
+    public void printPosition() {
+        char dir = 'N';
+        if (facing == 1) {
+            dir = 'N';
+        } else if (facing == 2) {
+            dir = 'E';
+        } else if (facing == 3) {
+            dir = 'S';
+        } else if (facing == 4) {
+            dir = 'W';
         }
-
-        private static String transform(String input, int rot, int position, int len) {
-            char[] ar = input.toCharArray();
-            for (int i = 0; i < rot; i++) {
-                rotate(ar, position - 1, len - position + 1);
-            }
-            return String.valueOf(ar);
+        System.out.println(x +  " "  + y  + " "+   dir);
+    }
+    public void process(String commands) {
+        for (int idx = 0; idx < commands.length(); idx++  ) {
+            process(commands.charAt(idx));
         }
-
-        private static void rotate(char arr[], int pos, int n) {
-            int l = pos;
-            int r = n - 1;
-            while (l < r) {
-                char temp = arr[l];
-                arr[l] = arr[r];
-                arr[r] = temp;
-                l++;
-                r--;
-            }
+    }
+    private void process(Character command) {
+        if (command.equals('L')) {
+            turnLeft();
+        } else if (command.equals('R')) {
+            turnRight();
+        } else if (command.equals('M')) {
+            move();
+        } else {
+            throw new IllegalArgumentException(
+                    "Speak in Mars language, please!");
         }
-    }*/
-
-
+    }
+    private void move() {
+        if (facing == N) {
+            this.y  ;
+        } else if (facing == E) {
+            this.x  ;
+        } else if (facing == S) {
+            this.y--;
+        } else if (facing == W) {
+            this.x--;
+        }
+    }
+    private void turnLeft() {
+        facing = (facing - 1) < N ? W : facing - 1;
+    }
+    private void turnRight() {
+        facing = (facing   1) > W ? N : facing   1;
+    }
+    public static void main(String args[]) {
+        Solution rover = new Solution();
+        rover.setPosition(1, 2, N);
+        rover.process("LMLMLMLMM");
+        rover.printPosition(); // prints 1 3 N
+        rover.setPosition(3, 3, E);
+        rover.process("MMRMMRMRRM");
+        rover.printPosition(); // prints 5 1 E
+    }
 }
